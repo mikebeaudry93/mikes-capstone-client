85e41644868cf90a8a8a17d9b5ab29f7fc98d940
Mike Beaudry <michaelbeaudry93@gmail.com>
2021-03-05 11:49:21 -0500

    css

--- src/components/accordian/Accordian.js
+++ src/components/accordian/Accordian.js
@@ -1,54 +1,55 @@
-import React, { useState, useRef } from 'react'
-import "./Accordian.scss"
+import React, { useState, useRef } from "react";
+import "./Accordian.scss";
 
-import Chevron from "../chevron/Chevron"
-import {Link} from 'react-router-dom'
-
+import Chevron from "../chevron/Chevron";
+import { Link } from "react-router-dom";
 
 export default function Accordian(props) {
+  const [setActive, setActiveState] = useState("");
+  const [setHeight, setHeightState] = useState("0px");
+  const [setRotate, setRotateState] = useState("accordian__icon");
 
-    const [setActive, setActiveState] = useState("");
-    const [setHeight, setHeightState] = useState("0px");
-    const [setRotate, setRotateState] = useState("accordian__icon");
+  const content = useRef(null);
 
-    const content = useRef(null);
+  function toggleMenu() {
+    setActiveState(setActive === "" ? "active" : "");
+    setHeightState(
+      setActive === "active" ? "0px" : `${content.current.scrollHeight}px`
+    );
+    setRotateState(
+      setActive === "active" ? "accordian__icon" : "accordian__icon rotate"
+    );
+  }
 
-    function toggleMenu () {
-        setActiveState(setActive === "" ? "active" : "");
-        setHeightState(
-            setActive === "active" ? "0px" : `${content.current.scrollHeight}px`
-            );
-            setRotateState(setActive === "active" ? "accordian__icon" : "accordian__icon rotate");
-    }
+  console.log(props);
 
-    console.log(props)
-
-    return (
-        
-        <div className="accordian__section">
-            <button className={`accordian ${setActive}`} onClick={toggleMenu}>
-                <p className="accordian__title">{props.itemContent.excercise}</p>
-                <p className="accordian__sets">{props.itemContent.sets}</p>
-                <p className="accordian__reps">{props.itemContent.reps}</p>
-                <Chevron className={`${setRotate}`} width={10} fill={"#777"}/>
-            </button>
-            <div  ref={content} style={{ maxHeight: `${setHeight}`}} className="accordian__content">
-                <div className="accordian__text"
-                dangerouslySetInnerHTML = {{ __html: props.itemContent.description }} />
-                    <div className="minutes-checkbox">
-                        <p className="time">{props.itemContent.time} minutes</p>
-                        <div class="pretty p-icon p-round p-jelly">
-                            <input type="checkbox" />
-                            <div class="state p-primary">
-                                <i class="icon mdi mdi-check"></i>
-                                <label>Done</label>
-                            </div>
-                    </div>
-                </div>
-                 <Link className="link" to={`/video/${props.itemContent[props.prefferedInstructor]}`}>
-                    <p className="accordian__videolink">Video Link</p>
-                </Link>
-            </div>
+  return (
+    <div className="accordian__section">
+      <button className={`accordian ${setActive}`} onClick={toggleMenu}>
+        <p className="accordian__title">{props.itemContent.excercise}</p>
+        <p className="accordian__sets">{props.itemContent.sets}</p>
+        <p className="accordian__reps">{props.itemContent.reps}</p>
+        <Chevron className={`${setRotate}`} width={10} fill={"#777"} />
+      </button>
+      <div
+        ref={content}
+        style={{ maxHeight: `${setHeight}` }}
+        className="accordian__content"
+      >
+        <div
+          className="accordian__text"
+          dangerouslySetInnerHTML={{ __html: props.itemContent.description }}
+        />
+        <div className="minutes-checkbox">
+          <p className="time">{props.itemContent.time} minutes</p>
         </div>
-    )
+        <Link
+          className="link"
+          to={`/video/${props.itemContent[props.prefferedInstructor]}`}
+        >
+          <p className="accordian__videolink">Video Link</p>
+        </Link>
+      </div>
+    </div>
+  );
 }
--- src/components/video/Video.js
+++ src/components/video/Video.js
@@ -1,24 +1,29 @@
-import React, { Component } from 'react'
-import { Link } from 'react-router-dom'
-import './Video.scss'
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import "./Video.scss";
 
-import ReactPlayer from "react-player"
+import ReactPlayer from "react-player";
 
 export class Video extends Component {
-    render() {
-        console.log(this.props)
+  render() {
+    console.log(this.props);
 
-        return (
-            <div className="page videopage">
-                <div className="player-wrapper">
-                    <ReactPlayer className="video" url={`http://youtube.com/watch${this.props.location.search}`} width="100%" height="600px"/>
-                </div>
-                <Link to="/result">
-                        <button className="go-back back-video">Go Back</button>
-                </Link>
-            </div>
-        )
-    }
+    return (
+      <div className="page videopage">
+        <div className="player-wrapper">
+          <ReactPlayer
+            className="video"
+            url={`http://youtube.com/watch${this.props.location.search}`}
+            width="100%"
+            height="600px"
+          />
+        </div>
+        <Link className="go-back back-video" to="/result">
+          <button className="go-back button-quiz">Go Back</button>
+        </Link>
+      </div>
+    );
+  }
 }
 
-export default Video
+export default Video;
