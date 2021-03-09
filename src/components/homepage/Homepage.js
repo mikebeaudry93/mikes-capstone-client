935f74379bd4e9ef80bd4de8b8c337cff8d459e2
Mike Beaudry <michaelbeaudry93@gmail.com>
2021-03-08 22:52:53 -0500

    css

--- src/components/homepage/Homepage.js
+++ src/components/homepage/Homepage.js
@@ -2,23 +2,14 @@ import React, { Component } from "react";
 import "./Homepage.css";
 import axios from "axios";
 
-import ReactPlayer from "react-player";
-import { CSSTransition } from "react-transition-group";
-import { Link } from "react-router-dom";
-
 // pages
 import Slideshow from "../SlideShow/SlideShow";
-import Quiz from "../quiz/Quiz";
 
 //images
 
-import slide1 from "../../assets/carousel/image8.jpg";
-import slide2 from "../../assets/carousel/image15.jpg";
 import slide3 from "../../assets/carousel/image10.jpg";
-import slide4 from "../../assets/carousel/image4.jpg";
 import slide5 from "../../assets/carousel/image16.jpg";
 import slide6 from "../../assets/carousel/image11.jpg";
-import image7 from "../../assets/carousel/image7.jpg";
 
 const s = {
   container: "screenW screenH dGray col",
@@ -52,9 +43,6 @@ export class Homepage extends Component {
         this.setState({
           isLoading: false,
         });
-        // setInterval(function () {
-        //   console.log("hello world");
-        // }, 3000);
       }
     });
   };
@@ -63,7 +51,6 @@ export class Homepage extends Component {
     return (
       <div className="homepage">
         <div className={s.container}>
-          {/* <div className={s.header}>uFitness</div> */}
           <div className={s.main}>
             <div className={s.header}>uFitness</div>
             <Slideshow slides={slides} />
@@ -82,20 +69,7 @@ export class Homepage extends Component {
               </button>
             </div>
           </div>
-          {/* <div className={s.footer}>
-                        <Link to="/duration-page">
-                           <button className="button1 button buttonstart" value="start" name="start" 
-                            >Start Quiz</button>
-                        </Link>
-                        </div> */}
         </div>
-        {/* <div className="home__title-box">
-                        <h1 className="home__title">uFitness</h1>
-                        // <Link to="/duration-page">
-                        //     <button className="button1 button" value="start" name="start" 
-                        //     >Start Quiz</button>
-                        // </Link>
-                    </div> */}
       </div>
     );
   }
